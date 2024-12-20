1c2f245851204d6c608e429940ece3fa86c1f95d
marsyofie <marsyofie@gmail.com>
2024-12-20 16:17:50 +0700

    test push

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const stage = process.env.STAGE || "staging"
 console.log(`Hello my name is ${process.env.NAME}`)
-console.log(`This code execution on ${stage}`)
+console.log(`This code execution on ${stage}`);
 // process.exit(0)
